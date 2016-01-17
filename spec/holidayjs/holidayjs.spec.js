cadb6d0c5ad15f4b1e7e0603dab933f87badb92b
John Kor <john.kor@gmail.com>
2016-01-16 17:53:09 -0800

    added some more tests for basic file opening behaaviour

--- spec/holidayjs/holidayjs.spec.js
+++ spec/holidayjs/holidayjs.spec.js
@@ -1,14 +1,34 @@
 describe('HolidayJS', function() {
   var holidayjs = require('../../lib/holidayjs')
 
-  it('should open the appropriate country data file', function() {
-    holidayjs._openCountryData('ca', function(err, data) {
-      console.log(data)
+
+  it('should open the appropriate country data file', function(done) {
+    holidayjs._openCountryData('test', function(err, data) {
+      expect(data).not.toBe(null);
+      expect(data).not.toBe(undefined);
+      expect(data instanceof Array).toBe(true);
+
+      done()
     })
   })
 
-  it('should error if the country data file is not found', function() {
-    
+  it('should error if the country data file is not found', function(done) {
+    holidayjs._openCountryData('fake', function(err, data) {
+      expect(data).toBe(undefined);
+      expect(err).not.toBe(null);
+
+      done()
+    })
+  })
+
+  it('should force country code to lowercase', function(done) {
+    holidayjs._openCountryData('TEST', function(err, data) {
+      expect(data).not.toBe(null);
+      expect(data).not.toBe(undefined);
+      expect(data instanceof Array).toBe(true);
+
+      done()
+    })
   })
 
   it('should generate holidays off the override rule', function() {
